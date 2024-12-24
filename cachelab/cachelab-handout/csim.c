fcb2dd113942ff05f5a1bd6dec0af06d44257bf4
jsk135 <2974551961@qq.com>
2024-12-24 17:53:16 +0800

    尝试cachelab

--- cachelab/cachelab-handout/csim.c
+++ cachelab/cachelab-handout/csim.c
@@ -1,7 +1,14 @@
 #include "cachelab.h"
-
+struct Line{
+    int valid;
+    unsigned flag; 
+    int count;
+};
+typedef struct Line* Set;
+typedef Set* cache;
 int main()
 {
+
     printSummary(0, 0, 0);
     return 0;
 }
